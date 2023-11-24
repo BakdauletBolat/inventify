af5def2c039a748f033559dacae205e20e7f2792
djars500 <djars500@gmail.com>
2023-11-24 15:52:28 +0600

    perf(car): api for get modification and other details

---
+++ apps/car/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from apps.car.models.Model import ModelCar, ManufacturerType
+from apps.car.models.Modification import Modification, Engine
+
+
+class ManufacturerTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ManufacturerType
+        fields = '__all__'
+
+
+class ModelCarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModelCar
+        fields = '__all__'
+
+
+class EngineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Engine
+        fields = '__all__'
+
+
+class ModificationSerializer(serializers.ModelSerializer):
+    axleConfiguration = serializers.CharField()
+    engines = serializers.SerializerMethodField()
+    driveType = serializers.CharField()
+    gearType = serializers.CharField()
+    fuelType = serializers.CharField()
+    mileageType = serializers.CharField()
+    bodyType = serializers.CharField()
+    modelCar = ModelCarSerializer()
+
+    @staticmethod
+    def get_engines(obj):
+        return obj.engine.all()
+
+    class Meta:
+        model = Modification
+        fields = '__all__'
---
+++ apps/car/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from apps.car import views
+
+urlpatterns = [
+    path('modifications/', views.ProductModificationListAPIView.as_view()),
+    path('manufacturers/', views.ManufacturerListAPIView.as_view()),
+    path('models/', views.CarModelsListAPIView.as_view()),
+    path('engines/', views.EnginesListAPIView.as_view()),
+]
