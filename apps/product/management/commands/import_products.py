cffe727e6bc3f0ca0ae7dd0011de4cd2bfad998c
djars500 <djars500@gmail.com>
2024-07-31 14:19:50 +0500

    fix(product): change size quality

--- apps/product/management/commands/import_products.py
+++ apps/product/management/commands/import_products.py
@@ -1,4 +1,5 @@
-from celery import group
+import time
+
 from django.core.management import BaseCommand
 
 from apps.product.enums import StatusChoices
@@ -21,17 +22,8 @@ def create_products():
     products = Product.objects.filter(id__in=product_ids_recar).values_list('id', flat=True)
     difference_products_ids = set(product_ids_recar).difference(products)
     remains_recar_products = ImportProductData.objects.filter(product_id__in=difference_products_ids)
-    batch_size = 1000  # Размер партии
-    tasks = []
-
-    for product_data in remains_recar_products:
-        tasks.append(import_product_task.s(product_data.id))
-        if len(tasks) >= batch_size:
-            job = group(tasks)
-            job.apply_async()
-            tasks = []  # Очистите список задач после отправки партии
-
-    # Отправьте оставшиеся задачи, если они есть
-    if tasks:
-        job = group(tasks)
-        job.apply_async()
+    delay = 5
+    for i, product_data in enumerate(remains_recar_products):
+        import_product_task.delay(product_data.id)
+        if i % 10 == 0:
+            time.sleep(delay)
