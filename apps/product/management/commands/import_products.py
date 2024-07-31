80f8ebd5a55784872ee4fbc637d2d43dda7b1eb8
djars500 <djars500@gmail.com>
2024-07-31 14:22:20 +0500

    fix(product): change size quality

--- apps/product/management/commands/import_products.py
+++ apps/product/management/commands/import_products.py
@@ -21,9 +21,15 @@ def create_products():
     product_ids_recar = ImportProductData.objects.values_list('product_id', flat=True)
     products = Product.objects.filter(id__in=product_ids_recar).values_list('id', flat=True)
     difference_products_ids = set(product_ids_recar).difference(products)
-    remains_recar_products = ImportProductData.objects.filter(product_id__in=difference_products_ids)
-    delay = 5
-    for i, product_data in enumerate(remains_recar_products):
-        import_product_task.delay(product_data.id)
-        if i % 10 == 0:
-            time.sleep(delay)
+    batch_size = 10
+    interval = 5  # Интервал между отправками задач в секундах
+
+    for start in range(0, len(difference_products_ids), batch_size):
+        end = start + batch_size
+        batch_ids = list(difference_products_ids)[start:end]
+        remains_recar_products = ImportProductData.objects.filter(product_id__in=batch_ids)
+
+        for product_data in remains_recar_products:
+            import_product_task.delay(product_data.id)
+
+        time.sleep(interval)  # Задержка между пакетами задач
