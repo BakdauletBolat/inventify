f20de8edd22227450ef71e16e3a8b463a702ae36
djars500 <djars500@gmail.com>
2024-10-07 17:57:32 +0500

    fix(stock): import model car if does not exist

--- apps/stock/tasks.py
+++ apps/stock/tasks.py
@@ -8,14 +8,19 @@ from base.requests import RecarRequest
 
 
 @shared_task
-def import_warehouse_detail():
+def import_warehouse_details():
     page = 1
     size = 50
     Stock.objects.all().delete()
     for pageNumber in range(page, 110, 1):
         warehouses = RecarRequest().get_warehouses(pageNumber, size)
         for warehouse in warehouses:
-            ImportWarehouseAction().import_detail(warehouse['id'])
+            import_warehouse_detail.delay(warehouse['id'])
+
+
+@shared_task
+def import_warehouse_detail(warehouse_id):
+    ImportWarehouseAction().import_detail(warehouse_id)
 
 
 @shared_task
@@ -33,4 +38,4 @@ def import_warehouse():
 def import_warehouses_from_recar():
     import_warehouse.delay()
     time.sleep(300)
-    import_warehouse_detail.delay()
+    import_warehouse_details.delay()
