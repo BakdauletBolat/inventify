04b7b61e5357d68932f064ac154590d435abbf34
djars500 <djars500@gmail.com>
2025-10-24 14:32:12 +0500

    fix(reacr): fix import pictures from recar

--- apps/product/actions.py
+++ apps/product/actions.py
@@ -86,7 +86,7 @@ class ImportProductAction:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         }
         for product_image in pictures:
-            image_url = product_image['optimized']
+            image_url = product_image['host'] + '1024/768/' + product_image['url']
             response = requests.get(image_url, headers=headers)
             image = Image.open(BytesIO(response.content))
 
