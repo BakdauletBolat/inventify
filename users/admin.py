e9036ba73d6cc4c7b63e5a8e0446ed1c07747c6b
djars500 <djars500@gmail.com>
2026-06-27 23:43:26 +0500

    feat(users): admin action to reset password to default

--- users/admin.py
+++ users/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 
 from users.models.User import User, Role
 from users.otp.models import UserCode
+from users.services.reset_password import ResetPasswordService
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'phone', 'first_name', 'status')
+    list_display = ('id', 'phone', 'first_name', 'email', 'status')
+    actions = ['reset_password']
+
+    @admin.action(description="Сбросить пароль на Zz123456")
+    def reset_password(self, request, queryset):
+        for user in queryset:
+            ResetPasswordService.reset_to_default(user)
+        self.message_user(request, f"Пароль сброшен у {queryset.count()} пользователей.")
 
 
 # Register your models here.
